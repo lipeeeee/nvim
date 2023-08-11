6e7886305fb118f56f1091b8bfd2280fd8e5d985
lipeeeee <filipe.pereirafga@gmail.com>
2023-08-11 23:25:52 +0100

    plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- Preferences
 require "lipe.preferences.options"
 require "lipe.preferences.keymaps"
 
+-- Plugins
+require "lipe.plugins.init_plugins"
---
+++ lua/lipe/plugins/plugins.lua
@@ -0,0 +1,8 @@
+-- All plugins
+return {
+  {"wbthomason/packer.nvim"},
+  {"nvim-lua/popup.nvim"},
+  {"nvim-lua/plenary.nvim"} 
+}
+
+
