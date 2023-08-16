331b2b3c61959579f09b539eacf04112919b98c9
lipeeeee <filipe.pereirafga@gmail.com>
2023-08-16 22:11:18 +0100

    broken, see last commit, starting over with lazy.nvim

--- init.lua
+++ init.lua
@@ -7,7 +7,20 @@ require(USR .. ".globals")
 require(USR .. ".preferences")
 
 -- Plugins
-require(USR .. ".plugins")
+-- Setup lazy first..
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({{import = "lipe.plugins"}})
 
 -- Completion
 require(USR .. ".cmp")
