5e62703fe4806bf0dac11ed534c87e85dfbdb274
lipeeeee <filipe.pereirafga@gmail.com>
2023-09-12 23:28:22 +0100

    cmp.lua cleaning

--- lua/lipe/plugins/lsp/cmp.lua
+++ lua/lipe/plugins/lsp/cmp.lua
@@ -17,40 +17,11 @@ return {
         return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
       end
 
-      local kind_icons = {
-        Text = "󰉿",
-        Method = "󰆧",
-        Function = "󰊕",
-        Constructor = "",
-        Field = " ",
-        Variable = "󰀫",
-        Class = "󰠱",
-        Interface = "",
-        Module = "",
-        Property = "󰜢",
-        Unit = "󰑭",
-        Value = "󰎠",
-        Enum = "",
-        Keyword = "󰌋",
-        Snippet = "",
-        Color = "󰏘",
-        File = "󰈙",
-        Reference = "",
-        Folder = "󰉋",
-        EnumMember = "",
-        Constant = "󰏿",
-        Struct = "",
-        Event = "",
-        Operator = "󰆕",
-        TypeParameter = " ",
-        Misc = " ",
-      }
-
       -- find more here: https://www.nerdfonts.com/cheat-sheet
       cmp.setup {
         snippet = {
           expand = function(args)
-            luasnip.lsp_expand(args.body) -- For `luasnip` users.
+            luasnip.lsp_expand(args.body)
           end,
         },
         mapping = {
@@ -106,18 +77,6 @@ return {
 
             return kind
           end,
-          -- format = function(entry, vim_item)
-          --   -- Kind icons
-          --   vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
-          --   -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-          --   vim_item.menu = ({
-          --     nvim_lsp = "(LSP)",
-          --     luasnip = "(Snippet)",
-          --     buffer = "(Buffer)",
-          --     path = "(Path)",
-          --   })[entry.source.name]
-          --   return vim_item
-          -- end,
         },
         sources = {
           { name = "nvim_lsp" },
