4d7fe037cf9342749fad2e37feda67b825541014
lipeeeee <filipe.pereirafga@gmail.com>
2023-10-11 22:55:02 +0100

    removed top line & auto exec to support wqa capital variants

--- lua/lipe/preferences/autoexec.lua
+++ lua/lipe/preferences/autoexec.lua
@@ -10,3 +10,13 @@ vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
 
 -- Disable command line history
 vim.cmd [[nnoremap q: <nop>]]
+
+-- Remap capital wq's to wq
+vim.cmd [[command! W w]]
+vim.cmd [[command! WQ wq]]
+vim.cmd [[command! Wq wq]]
+vim.cmd [[command! WA wa]]
+vim.cmd [[command! Wa wa]]
+vim.cmd [[command! WQA wqa]]
+vim.cmd [[command! WQa wqa]]
+vim.cmd [[command! Wqa wqa]]
--- test/main.c
+++ test/main.c
@@ -1,5 +1,4 @@
 int main(int argc, char *argv[]) {
-
   if ("ola") {
     int ola = 21;
     return 0;
