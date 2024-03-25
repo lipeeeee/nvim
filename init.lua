e1339454f635a2b91c3baf720224ff5ac7b25c6b
lipeeeee <filipe.pereirafga@gmail.com>
2024-03-25 19:32:45 +0000

    test fugitive

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ local function init_environment()
 
   -- Globals
   require("lipe.globals")
-  FILE_EXPLORER_TO_USE = NVIM_TREE
+  FILE_EXPLORER_TO_USE = NETRW
 
   -- Options
   require("lipe.preferences.options")
