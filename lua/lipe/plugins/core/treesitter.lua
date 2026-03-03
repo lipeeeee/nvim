314fd3db44021a8285168de40ae728e68a4214ce
lipeeeee <filipe.pereirafga@gmail.com>
2026-03-03 19:01:33 +0000

    xdd version update

--- lua/lipe/plugins/core/treesitter.lua
+++ lua/lipe/plugins/core/treesitter.lua
@@ -8,33 +8,19 @@ end
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    event = "BufEnter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
     config = function()
-      local ts_config = require("nvim-treesitter.configs")
-
-      ts_config.setup {
-        -- A list of parser names, or "all" (the five listed parsers should always be installed)
+      require("nvim-treesitter").setup {
         ensure_installed = { "c", "lua", "python" },
-
-        -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
         auto_install = true,
-
-        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
         -- Indentation
         indent = { enable = true },
 
         highlight = {
           enable = true,
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
           additional_vim_regex_highlighting = false,
           disable = { "rust" },
         },
@@ -42,7 +28,7 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = "gnn", -- set to `false` to disable one of the mappings
+            init_selection = "gnn",
             node_incremental = "grn",
             scope_incremental = "grc",
             node_decremental = "grm",
@@ -53,8 +39,5 @@ return {
       -- Fix dumb italic defaults
       remove_italics()
     end
-  },
-  {
-    "nvim-treesitter/playground", event = "VeryLazy"
   }
 }
